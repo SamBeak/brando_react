ef90d5d49f4afe0a9c914f76aa7422035491df98
Baek HyunGyu <baekhg102@gmail.com>
2023-07-08 23:12:03 +0900

    docs: Add HeaderComponent

--- src/components/WrapComponent.jsx
+++ src/components/WrapComponent.jsx
@@ -2,16 +2,18 @@ import React from 'react'
 // import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 // Component List
-import FooterComponent from './wrap/FooterComponent';
+import SkipComponent from './wrap/SkipComponent';
 import HeaderComponent from './wrap/HeaderComponent';
 import HomeCompoent from './wrap/HomeComponent';
+import FooterComponent from './wrap/FooterComponent';
 
 export default function WrapComponent() {
   return (
     <div id="wrap">
-        <HeaderComponent />
-        <HomeCompoent />
-        <FooterComponent />
+      <SkipComponent />
+      <HeaderComponent />
+      <HomeCompoent />
+      <FooterComponent />
     </div>
   )
 }
--- src/components/wrap/HeaderComponent.jsx
+++ src/components/wrap/HeaderComponent.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import React from 'react';
 
 export default function HeaderComponent() {
   return (
-    <header id="header"></header>
+    <header id="header">
+      <div className="container">
+        <div className="gap">
+          <div className="navbar-left">
+            <ul className="navbar-left__list">
+              <li className="navbar-left__item"><a href="#section2" className="navbar-left__link" title="THE COUPLE">THE COUPLE</a></li>
+              <li className="navbar-left__item"><a href="#section4" className="navbar-left__link" title="EVENT & PARTIES">EVENT & PARTIES</a></li>
+              <li className="navbar-left__item"><a href="#section5" className="navbar-left__link" title="GALLERY">GALLERY</a></li>
+            </ul>
+          </div>
+          <div className="navbar-center">
+            <a href="#wrap" className="navbar-center__link blind">Home</a>
+          </div>
+          <div className="navbar-right">
+            <ul className="navbar-right__list">
+              <li className="navbar-right__item"><a href="#section6" className="navbar-right__link" title='ATTENDING'>ATTENDING</a></li>
+              <li className="navbar-right__item"><a href="#section9" className="navbar-right__link" title='OUR BLOG'>OUR BLOG</a></li>
+              <li className="navbar-right__item"><a href="#section10" className="navbar-right__link" title='LOCATION'>LOCATION</a></li>
+            </ul>
+          </div>
+          <div className="navbar-mobile">
+            <a href="!#" className="navbar-mobile__btn">
+                <i className="line"></i>
+                <i className="line"></i>
+                <i className="line"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+    </header>
   )
 }
