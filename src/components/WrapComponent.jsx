e025d5c611de7060aab93d465f29249eff031a58
Baek HyunGyu <baekhg102@gmail.com>
2023-07-08 21:56:43 +0900

    docs: Add reset.css and npm

--- src/components/WrapComponent.jsx
+++ src/components/WrapComponent.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 // import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 // Component List
-import FooterComponent from './wrap/FooterComponent'
-import HeaderComponent from './wrap/HeaderComponent'
-import MainComponent from './wrap/MainComponent'
+import FooterComponent from './wrap/FooterComponent';
+import HeaderComponent from './wrap/HeaderComponent';
+import HomeCompoent from './wrap/HomeComponent';
 
 export default function WrapComponent() {
   return (
     <div id="wrap">
         <HeaderComponent />
-        <MainComponent />
+        <HomeCompoent />
         <FooterComponent />
     </div>
   )
---
+++ src/components/wrap/HomeComponent.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function HomeCompoent() {
+  return (
+    <main id="home"></main>
+  )
+}
