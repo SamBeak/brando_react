916ff529610e97017e0ae85fca922ae0c3a9dee5
Baek HyunGyu <baekhg102@gmail.com>
2023-07-08 22:24:35 +0900

    docs: Add SkipComponent

---
+++ src/components/wrap/SkipComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function SkipComponent() {
+  return (
+    <ul id="skip">
+        <li><a href="#header">헤더바로가기</a></li>
+        <li><a href="#home">메인바로가기</a></li>
+        <li><a href="#footer">하단바로가기</a></li>
+    </ul>
+  )
+}
