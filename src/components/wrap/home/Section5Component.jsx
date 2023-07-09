01b23a4c7fc01c84b47167e597e8066308e8bddb
Baek HyunGyu <baekhg102@gmail.com>
2023-07-09 20:24:59 +0900

    docs: Add section5component

---
+++ src/components/wrap/home/Section5Component.jsx
@@ -0,0 +1,91 @@
+import React from 'react';
+
+export default function Section5Component() {
+  return (
+    <section id="section5">
+        <div className="container">
+            <div className="gap">
+                <div className="title"><h2>PHOTO GALLERY</h2></div>
+                <div className="content">
+                    <div className="gallery-box">
+                        <ul>
+                            <li>
+                                <div className="col-gap">
+                                    <a href="!#">
+                                        <figure>
+                                            <img src="./images/wedding-img8.jpg" alt="웨딩 사진" />
+                                        </figure>                                            
+                                    </a>
+                                </div>
+                            </li>
+                            <li>
+                                <div className="col-gap">
+                                    <a href="!#">
+                                        <figure>
+                                            <img src="./images/wedding-img9.jpg" alt="웨딩 사진" />
+                                        </figure>                                            
+                                    </a>
+                                </div>
+                            </li>
+                            <li>
+                                <div className="col-gap">
+                                    <a href="!#">
+                                        <figure>
+                                            <img src="./images/wedding-img10.jpg" alt="웨딩 사진" />
+                                        </figure>                                            
+                                    </a>
+                                </div>
+                            </li>
+                            <li>
+                                <div className="col-gap">
+                                    <a href="!#">
+                                        <figure>
+                                            <img src="./images/wedding-img11.jpg" alt="웨딩 사진" />
+                                        </figure>                                            
+                                    </a>
+                                </div>
+                            </li>
+                            <li>
+                                <div className="col-gap">
+                                    <a href="!#">
+                                        <figure>
+                                            <img src="./images/wedding-img12.jpg" alt="웨딩 사진" />
+                                        </figure>                                            
+                                    </a>
+                                </div>
+                            </li>
+                            <li>
+                                <div className="col-gap">
+                                    <a href="!#">
+                                        <figure>
+                                            <img src="./images/wedding-img13.jpg" alt="웨딩 사진" />
+                                        </figure>                                            
+                                    </a>
+                                </div>
+                            </li>
+                            <li>
+                                <div className="col-gap">
+                                    <a href="!#">
+                                        <figure>
+                                            <img src="./images/wedding-img14.jpg" alt="웨딩 사진" />
+                                        </figure>                                            
+                                    </a>
+                                </div>
+                            </li>
+                            <li>
+                                <div className="col-gap">
+                                    <a href="!#">
+                                        <figure>
+                                            <img src="./images/wedding-img15.jpg" alt="웨딩 사진" />
+                                        </figure>                                            
+                                    </a>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
